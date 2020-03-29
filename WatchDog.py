72e18f2efb2fe5fc48818ff7b5371a89dc44788f
arm-star <55631682+arm-star@users.noreply.github.com>
2020-03-29 17:04:17 +0200

    added comments to WatchDog

--- WatchDog.py
+++ WatchDog.py
@@ -3,11 +3,11 @@ from Consumer import Consumer
 from definitions import *
 
 if __name__ == "__main__":
-    obs = Observer(PATH_TO_WATCH)
-    s1 = Consumer('User_1')
-    s1.configure(FILES_LIST_USER_1)
+    obs = Observer(PATH_TO_WATCH)  # create observer instance
+    s1 = Consumer('User_1')         # create consumer instance
+    s1.configure(FILES_LIST_USER_1)    # configure consumer instance
     s2 = Consumer('User_2')
     s2.configure(FILES_LIST_USER_2)
-    obs.add_consumer(s1)
+    obs.add_consumer(s1)                  # add consumer to observer
     obs.add_consumer(s2)
     obs.run()
